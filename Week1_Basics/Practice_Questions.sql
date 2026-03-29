b0e7e619316812e597385bfd5777254bb4ab14e9
vishakha251-sri <svishakha251@gmail.com>
2026-03-30 02:39:59 +0530

    Add Topic 2: AND & OR Practice ✅

--- Week1_Basics/Practice_Questions.sql
+++ Week1_Basics/Practice_Questions.sql
@@ -27,3 +27,40 @@ SELECT *
 FROM employees
 WHERE age < 25;
 
+
+-- ------------------------------------------------
+-- TOPIC 2: AND & OR
+-- ------------------------------------------------
+
+-- Q1: Show all employees from Delhi whose age is greater than 25
+SELECT *
+FROM employees
+WHERE city = 'Delhi' AND age > 25;
+
+-- Q2: Show all employees from Mumbai or Bangalore
+SELECT *
+FROM employees
+WHERE city = 'Mumbai' OR city = 'Bangalore';
+
+
+-- Q3: Show employees from IT department whose salary is greater than 50000
+SELECT *
+FROM employees
+WHERE department = 'IT' AND salary > 50000;
+
+-- Q4: Show employees who are either from Delhi or have salary greater than 60000
+SELECT *
+FROM employees
+WHERE city = 'Delhi' OR salary > 60000;
+
+-- Q5: Show employees from HR department whose age is less than 25 or greater than 30
+SELECT *
+FROM employees
+WHERE department = 'HR' AND (age < 25 OR age > 30);
+
+
+
+
+
+
+
